f8a827d1a44103b78faa5aa6600792374a86dce7
simplehasan <inuinsane2@gmail.com>
2023-08-26 12:47:05 +0700

    create topbar and sidebar

--- src/App.js
+++ src/App.js
@@ -1,5 +1,19 @@
 import { ColorModeContext, useMode } from "./theme";
 import { CssBaseline, ThemeProvider } from "@mui/material";
+import Topbar from "./scenes/global/Topbar";
+import Sidebar from "./scenes/global/Sidebar";
+import Dashboard from "./scenes/dashboard/";
+// import Team from "./scenes/Team";
+// import Invoices from "./scenes/Invoices";
+// import Contacts from "./scenes/Contacts";
+// import Bar from "./scenes/Bar";
+// import Form from "./scenes/Form";
+// import Line from "./scenes/Line";
+// import Pie from "./scenes/Pie";
+// import FAQ from "./scenes/Faq";
+// import Geography from "./scenes/Geography";
+// import Calendar from "./scenes/Calendar";
+import { Route, Routes } from "react-router-dom";
 
 function App() {
   const [theme, colorMode] = useMode();
@@ -10,7 +24,23 @@ function App() {
         <ThemeProvider theme={theme}>
           <CssBaseline />
           <div className="app">
-            <main className="content"></main>
+            <Sidebar />
+            <main className="content">
+              <Topbar />
+              <Routes>
+                <Route path="/" element={<Dashboard />} />
+                {/* <Route path="/team" element={<Team />} /> */}
+                {/* <Route path="/contacts" element={<Contacts />} /> */}
+                {/* <Route path="/invoices" element={<Invoices />} /> */}
+                {/* <Route path="/form" element={<Form />} /> */}
+                {/* <Route path="/bar" element={<Bar />} /> */}
+                {/* <Route path="/pie" element={<Pie />} /> */}
+                {/* <Route path="/line" element={<Line />} /> */}
+                {/* <Route path="/faq" element={<FAQ />} /> */}
+                {/* <Route path="/geography" element={<Geography />} /> */}
+                {/* <Route path="/calendar" element={<Calendar />} /> */}
+              </Routes>
+            </main>
           </div>
         </ThemeProvider>
       </ColorModeContext.Provider>
--- src/theme.js
+++ src/theme.js
@@ -1,11 +1,10 @@
 import { createContext, useState, useMemo } from "react";
 import { createTheme } from "@mui/material/styles";
 
-// color design tokens
+// color design tokens export
 export const tokens = (mode) => ({
   ...(mode === "dark"
     ? {
-        // Dark Theme
         grey: {
           100: "#e0e0e0",
           200: "#c2c2c2",
@@ -21,7 +20,7 @@ export const tokens = (mode) => ({
           100: "#d0d1d5",
           200: "#a1a4ab",
           300: "#727681",
-          400: "#434957",
+          400: "#1F2A40",
           500: "#141b2d",
           600: "#101624",
           700: "#0c101b",
@@ -63,7 +62,6 @@ export const tokens = (mode) => ({
         },
       }
     : {
-        // Light Theme
         grey: {
           100: "#141414",
           200: "#292929",
@@ -122,81 +120,80 @@ export const tokens = (mode) => ({
       }),
 });
 
-// material ui theme settings
+// mui theme settings
 export const themeSettings = (mode) => {
   const colors = tokens(mode);
+  return {
+    palette: {
+      mode: mode,
+      ...(mode === "dark"
+        ? {
+            // palette values for dark mode
+            primary: {
+              main: colors.primary[500],
+            },
+            secondary: {
+              main: colors.greenAccent[500],
+            },
+            neutral: {
+              dark: colors.grey[700],
+              main: colors.grey[500],
+              light: colors.grey[100],
+            },
+            background: {
+              default: colors.primary[500],
+            },
+          }
+        : {
+            // palette values for light mode
+            primary: {
+              main: colors.primary[100],
+            },
+            secondary: {
+              main: colors.greenAccent[500],
+            },
+            neutral: {
+              dark: colors.grey[700],
+              main: colors.grey[500],
+              light: colors.grey[100],
+            },
+            background: {
+              default: "#fcfcfc",
+            },
+          }),
+    },
+    typography: {
+      fontFamily: ["Source Sans Pro", "sans-serif"].join(","),
+      fontSize: 12,
+      h1: {
+        fontFamily: ["Source Sans Pro", "sans-serif"].join(","),
+        fontSize: 40,
+      },
+      h2: {
+        fontFamily: ["Source Sans Pro", "sans-serif"].join(","),
+        fontSize: 32,
+      },
+      h3: {
+        fontFamily: ["Source Sans Pro", "sans-serif"].join(","),
+        fontSize: 24,
+      },
+      h4: {
+        fontFamily: ["Source Sans Pro", "sans-serif"].join(","),
+        fontSize: 20,
+      },
+      h5: {
+        fontFamily: ["Source Sans Pro", "sans-serif"].join(","),
+        fontSize: 16,
+      },
+      h6: {
+        fontFamily: ["Source Sans Pro", "sans-serif"].join(","),
+        fontSize: 14,
+      },
+    },
+  };
 };
 
-return {
-  palette: {
-    mode: mode,
-    ...(mode === "dark"
-      ? {
-          // dark mode color
-          primary: {
-            main: colors.primary[500],
-          },
-          secondary: {
-            main: colors.greenAccent[500],
-          },
-          neutral: {
-            main: colors.grey[500],
-            dark: colors.grey[700],
-            light: colors.grey[100],
-          },
-          background: {
-            default: colors.primary[500],
-          },
-        }
-      : {
-          // light mode color
-          primary: {
-            main: colors.primary[100],
-          },
-          secondary: {
-            main: colors.greenAccent[500],
-          },
-          neutral: {
-            main: colors.grey[500],
-            dark: colors.grey[700],
-            light: colors.grey[100],
-          },
-          background: {
-            default: "#fcfcfc",
-          },
-        }),
-  },
-  typography: {
-    fontFamily: ["Source Sans 3", "sabs-serif"].join(","),
-    fontSize: 12,
-    h1: {
-      fontFamily: ["Source Sans 3", "sabs-serif"].join(","),
-      fontSize: 40,
-    },
-    h2: {
-      fontFamily: ["Source Sans 3", "sabs-serif"].join(","),
-      fontSize: 32,
-    },
-    h3: {
-      fontFamily: ["Source Sans 3", "sabs-serif"].join(","),
-      fontSize: 24,
-    },
-    h4: {
-      fontFamily: ["Source Sans 3", "sabs-serif"].join(","),
-      fontSize: 20,
-    },
-    h5: {
-      fontFamily: ["Source Sans 3", "sabs-serif"].join(","),
-      fontSize: 16,
-    },
-    h6: {
-      fontFamily: ["Source Sans 3", "sabs-serif"].join(","),
-      fontSize: 14,
-    },
-  },
-};
-
-// Context for color mode
+// context for color mode
 export const ColorModeContext = createContext({
   toggleColorMode: () => {},
 });
@@ -204,12 +201,14 @@ export const ColorModeContext = createContext({
 export const useMode = () => {
   const [mode, setMode] = useState("dark");
 
-  const colorMode = useMemo(() => {
-    toggleColorMode: () =>
-      setMode((prev) => (prev === "light" ? "dark" : "light"));
-  }, []);
+  const colorMode = useMemo(
+    () => ({
+      toggleColorMode: () =>
+        setMode((prev) => (prev === "light" ? "dark" : "light")),
+    }),
+    []
+  );
 
   const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
-
   return [theme, colorMode];
 };
